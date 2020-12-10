7f7c43b90f85ae5647555d0bed370c02d74ded8f
ajurna <ajurna@gmail.com>
2020-12-10 22:41:54 +0000

    day 10 optimisation

--- day10/10.py
+++ day10/10.py
@@ -1,4 +1,4 @@
-from collections import deque
+from functools import cache
 
 with open('01.txt', 'r') as f:
     data = [int(x.strip()) for x in f.readlines()]
@@ -14,27 +14,18 @@ for low, high in zip(data, data[1:]):
         ones += 1
 print('Part 1:', ones*threes)
 
-index = {x: [y for y in data if y>x and y-x <= 3] for x in data}
-
-results = {}
+index = {x: [y for y in data if y > x and y-x <= 3] for x in data}
 
 
+@cache
 def search(num, endpoint):
     total = 0
-    queue = deque([num])
-    while queue:
-        current = queue.popleft()
-        for n in index[current]:
-            if n == endpoint:
-                total += 1
-            elif n in results:
-                total += results[n]
-            else:
-                queue.append(n)
+    for n in index[num]:
+        if n == endpoint:
+            total += 1
+        else:
+            total += search(n, endpoint)
     return total
 
 
-for i in sorted(data, reverse=True)[1:]:
-    results[i] = search(i, max(data))
-
-print('Part 2:', results[0])
+print('Part 2:', search(0, max(data)))
